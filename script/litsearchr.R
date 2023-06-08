7bfcba1ffb008020fabf3dde0fff9650941a1536
Limoilou Renaud <limoilou.renaud@gmail.com>
2023-06-08 19:36:46 -0400

    renaming project and starting litsearchr

---
+++ script/litsearchr.R
@@ -0,0 +1,7 @@
+# script to systematic review of articles in Scopus using litsearchr
+
+library(tidyverse)
+library(devtools)
+
+install_github("elizagrames/litsearchr", ref="main")
+library(litsearchr)
